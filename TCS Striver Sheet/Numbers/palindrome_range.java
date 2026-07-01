8c465abc9ed4a63ad149d36fbc0f1bcbcda239c2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-07-01 20:43:52 +0530

    start range added

--- TCS Striver Sheet/Numbers/palindrome_range.java
+++ TCS Striver Sheet/Numbers/palindrome_range.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class palindrome_range{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
         int ctr = 0;
-        for(int i=1 ; i<=n ; i++){
+        for(int i=n1 ; i<=n2 ; i++){
             int rev = 0 ;
             int num = i;
             while(num>0){
