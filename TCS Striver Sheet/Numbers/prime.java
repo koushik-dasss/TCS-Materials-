a1746914b3b0b9893d56fd5675c7c4f691cd9c5d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-07-01 20:38:18 +0530

    prime

---
+++ TCS Striver Sheet/Numbers/prime.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean result = true;
+        if(n<2){
+            result = false;
+            System.out.println("Prime: " + result);
+        }else{
+            for(int i=2 ; i*i<=n ; i++){
+                if(n%i==0){
+                    result = false;
+                    break;
+                }
+            }
+        }
+        System.out.println("Prime: "+ result );
+        sc.close();
+    }
+    
+}
